936b86105d9119a084f75160c6b4f7d517cd1df5
Yuren Ju <yurenju@gmail.com>
2026-05-10 16:29:31 +0900

    Add toggle-panel command and icon click

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,6 @@
 import type { Message, CaptureTabResponse } from './messages';
 import { captureActiveTab } from './screenshot';
+import { sendTogglePanel } from './commands';
 
 chrome.runtime.onMessage.addListener((msg: Message, _sender, sendResponse) => {
   if (msg.type === 'capture-tab') {
@@ -9,4 +10,16 @@ chrome.runtime.onMessage.addListener((msg: Message, _sender, sendResponse) => {
     return true; // keep port open for async response
   }
   return false;
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.id) sendTogglePanel(tab.id);
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'toggle-panel') {
+    chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
+      if (tab?.id) sendTogglePanel(tab.id);
+    });
+  }
 });
